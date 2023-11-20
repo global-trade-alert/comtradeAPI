3130c90a638f7570a26ea479e10c587372e4d178
svensglinz <svenglinz@gmail.com>
2023-11-20 19:22:58 +0100

    changes

--- R/getBulkDataFromLink.R
+++ R/getBulkDataFromLink.R
@@ -13,6 +13,7 @@ getBulkDataFromLink <- function(
 
     defaultTimeout <- getOption("timeout")
     options(timeout = timeout)
+
     for (i in seq_along(data$fileUrl)) {
         tryCatch(
         {
@@ -20,9 +21,10 @@ getBulkDataFromLink <- function(
             file_url <- httr::modify_url(data$fileUrl[i], query = key_attr)
             entry <- data[i,]
             name <- glue::glue("{entry$reporterCode}_{entry$freqCode}_{entry$typeCode}_{entry$classificationCode}_{entry$period}")
+            sizeMsg <- cli::style_bold(glue::glue("downloading {data$fileSize[i]} ..."))
+            progressMsg <- cli::style_bold(cli::col_green(glue::glue("file {i}/{nrow(data)} downloading")))
+            cat("\r", sizeMsg, " ", progressMsg)
             download.file(file_url, destfile = glue::glue("{filePath}/{name}.txt.gz"), mode = "wb", quiet = TRUE)
-            msg <- cli::style_bold(cli::col_green(glue::glue("file {i}/{nrow(data)} downloaded")))
-            cat("\r", msg)
         },
             error = function(e) {
                 cli::cli_abort("Error in download of file {entry$reporterCode}_{entry$freqCode}_{entry$typeCode}_{entry$classificationCode}_{entry$period}")
--- R/getData.R
+++ R/getData.R
@@ -62,3 +62,4 @@ getData <- function(
     out <- apiRequest(base_url, attributes)
     return(out)
 }
+
